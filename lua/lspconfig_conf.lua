4fb599b929dc58ab4729ba1d67fbb3382d6e8b55
Maarten de Groot <m.l.d.groot@student.tue.nl>
2025-05-04 13:45:39 +0200

    fixed error signs

--- lua/lspconfig_conf.lua
+++ lua/lspconfig_conf.lua
@@ -82,6 +82,28 @@ require('lspconfig').harper_ls.setup {
   filetypes = {'typst'},
 }
 
+vim.diagnostic.config({
+        signs = {
+                text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN] = " ",
+                        [vim.diagnostic.severity.INFO] = "󰋼 ",
+                        [vim.diagnostic.severity.HINT] = "󰌵 ",
+                },
+                texthl = {
+                        [vim.diagnostic.severity.ERROR] = "Error",
+                        [vim.diagnostic.severity.WARN] = "Error",
+                        [vim.diagnostic.severity.HINT] = "Hint",
+                        [vim.diagnostic.severity.INFO] = "Info",
+                },
+                numhl = {
+                        [vim.diagnostic.severity.ERROR] = "",
+                        [vim.diagnostic.severity.WARN] = "",
+                        [vim.diagnostic.severity.HINT] = "",
+                        [vim.diagnostic.severity.INFO] = "",
+                },
+        },
+})
 -- require("nvim-treesitter.configs").setup({
 --   ensure_installed = { "c", "lua", "vim", "typst" , "python"},
 -- })
--- lua/nvim_navic_conf.lua
+++ lua/nvim_navic_conf.lua
@@ -1,11 +1,5 @@
 local navic = require("nvim-navic")
 
---local on_attach = function(client, bufnr)
---    if client.server_capabilities.documentSymbolProvider then
---        navic.attach(client, bufnr)
---    end
---end
-
 navic.setup {
     icons = {
         File          = "󰈙 ",
@@ -50,41 +44,3 @@ navic.setup {
         return text
     end,
 }
---navic.setup {
---    icons = {
---        File          = "󰈙 ",
---        Module        = " ",
---        Namespace     = "󰌗 ",
---        Package       = " ",
---        Class         = "󰌗 ",
---        Method        = "󰆧 ",
---        Property      = " ",
---        Field         = " ",
---        Constructor   = " ",
---        Enum          = "󰕘",
---        Interface     = "󰕘",
---        Function      = "󰊕 ",
---        Variable      = "󰆧 ",
---        Constant      = "󰏿 ",
---        String        = " ",
---        Number        = "󰎠 ",
---        Boolean       = "◩ ",
---        Array         = "󰅪 ",
---        Object        = "󰅩 ",
---        Key           = "󰌋 ",
---        Null          = "󰟢 ",
---        EnumMember    = " ",
---        Struct        = "󰌗 ",
---        Event         = " ",
---        Operator      = "󰆕 ",
---    },
---    highlight = true,
---    separator = " > ",
---    depth_limit = 0,
---    depth_limit_indicator = "..",
---    safe_output = true
---}
---
---on_attach = function(client, bufnr)
---    navic.attach(client, bufnr)
---end
